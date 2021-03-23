49a598520bb3a8f7d67f84da89696939ba81358f
Gina Sprint <sprint@gonzaga.edu>
2021-03-22 21:45:00 -0700

    start of PA6 starter code

---
+++ DecisionTreeFunS1/main.py
@@ -0,0 +1,35 @@
+header = ["level", "lang", "tweets", "phd"]
+attribute_domains = {"level": ["Senior", "Mid", "Junior"], 
+        "lang": ["R", "Python", "Java"],
+        "tweets": ["yes", "no"], 
+        "phd": ["yes", "no"]}
+X_train = [
+        ["Senior", "Java", "no", "no"],
+        ["Senior", "Java", "no", "yes"],
+        ["Mid", "Python", "no", "no"],
+        ["Junior", "Python", "no", "no"],
+        ["Junior", "R", "yes", "no"],
+        ["Junior", "R", "yes", "yes"],
+        ["Mid", "R", "yes", "yes"],
+        ["Senior", "Python", "no", "no"],
+        ["Senior", "R", "yes", "no"],
+        ["Junior", "Python", "yes", "no"],
+        ["Senior", "Python", "yes", "yes"],
+        ["Mid", "Python", "no", "yes"],
+        ["Mid", "Java", "yes", "no"],
+        ["Junior", "Python", "no", "yes"]
+    ]
+
+y_train = ["False", "False", "True", "True", "True", "False", "True", "False", "True", "True", "True", "True", "True", "False"]
+
+
+def tdidt(current_instances, available_attributes):
+    # basic approach (uses recursion!!):
+
+    # select an attribute to split on
+    # group data by attribute domains (creates pairwise disjoint partitions)
+    # for each partition, repeat unless one of the following occurs (base case)
+    #    CASE 1: all class labels of the partition are the same => make a leaf node
+    #    CASE 2: no more attributes to select (clash) => handle clash w/majority vote leaf node
+    #    CASE 3: no more instances to partition (empty partition) => backtrack and replace attribute node with majority vote leaf node
+    return None
