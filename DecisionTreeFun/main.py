131968d88eb170457cc9f0472ea7102eb4af4d0b
Gina Sprint <sprint@gonzaga.edu>
2022-11-03 17:29:15 +0000

    starter code through bases cases

--- DecisionTreeFun/main.py
+++ DecisionTreeFun/main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 X_train = [
         ["Senior", "Java", "no", "no"],
         ["Senior", "Java", "no", "yes"],
@@ -62,13 +64,77 @@ interview_tree_solution =   ["Attribute", "att0",
                                 ]
                             ]
 
+def select_attribute(instances, attributes):
+    # TODO: implement the Enew algorithm to select the
+    # attribute with the lowest entropy
+    # for now, let's use random selection
+    rand_index = np.random.randint(0, len(attributes))
+    return attributes[rand_index]
+
+def partition_instances(instances, attribute):
+    # this is a group by attribute domain
+    att_index = header.index(attribute)
+    att_domain = attribute_domains["att" + str(att_index)]
+    print("attribute domain:", att_domain)
+    # lets use dictionaries
+    partitions = {}
+    for att_value in att_domain:
+        partitions[att_value] = []
+        for instance in instances:
+            if instance[att_index] == att_value:
+                partitions[att_value].append(instance)
+    return partitions
+
 def tdidt(current_instances, available_attributes):
     # basic approach (uses recursion!!):
+    print("available attributes:", available_attributes)
 
     # select an attribute to split on
+    split_attribute = select_attribute(current_instances, available_attributes)
+    print("splitting on:", split_attribute)
+    available_attributes.remove(split_attribute)
+    # cannot split on this attribute again in this branch of tree
+    tree = ["Attribute", split_attribute]
+
     # group data by attribute domains (creates pairwise disjoint partitions)
+    partitions = partition_instances(current_instances, split_attribute)
+    print("partitions:", partitions)
+
     # for each partition, repeat unless one of the following occurs (base case)
-    #    CASE 1: all class labels of the partition are the same => make a leaf node
-    #    CASE 2: no more attributes to select (clash) => handle clash w/majority vote leaf node
-    #    CASE 3: no more instances to partition (empty partition) => backtrack and replace attribute node with majority vote leaf node
-    return None
+    for att_value, att_partition in partitions.items():
+        value_subtree = ["Value", att_value]
+        if len(att_partition) > 0 and same_class_label(att_partition):
+            print("CASE 1 all same class label")
+            #    CASE 1: all class labels of the partition are the same
+            # => make a leaf node
+        elif len(att_partition) > 0 and len(available_attributes) == 0:
+            print("CASE 2 clash")
+            #    CASE 2: no more attributes to select (clash)
+            # => handle clash w/majority vote leaf node
+        elif len(att_partition) == 0:
+            print("CASE 3 empty partition")
+            #    CASE 3: no more instances to partition (empty partition)
+            # => backtrack and replace attribute node with majority vote leaf node
+        else: # none of the bases cases were true...recurse!!
+            print("Recursing!!!")
+            subtree = tdidt(att_partition, available_attributes.copy())
+            value_subtree.append(subtree)
+        tree.append(value_subtree)
+
+    return tree
+
+def fit_starter_code():
+    # note the TODO above
+    # here would be a good place to programmatically extract
+    # the header and attribute_domains
+    # next, I recommend stitching X_train and y_train together
+    # so the class label is at instance[-1]
+    train = [X_train[i] + [y_train[i]] for i in range(len(X_train))]
+    available_attributes = header.copy() # recall tdidt is going
+    # to be removing attributes from a list of available attributes
+    # python is pass by object reference!!!
+    tree = tdidt(train, available_attributes)
+    print("tree:", tree)
+    # note the unit test will assert tree == interview_tree_solution
+
+fit_starter_code()
